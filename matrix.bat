8c0e6314d3ab85e9e090b7357bcddcf3c826027f
jezztech <admin@bsociable.org>
2013-10-25 22:24:01 +0100

    Create matrix.bat
    This is the most basic code you can do to make this work. The mode and color statements are optional.

---
+++ matrix.bat
@@ -0,0 +1,6 @@
+@echo off
+mode 1000
+color 0a
+:matrix
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto matrix
